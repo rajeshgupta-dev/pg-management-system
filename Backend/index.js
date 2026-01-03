eecdc23fe70d710feb527bd0d84ed9d83507c9e3
rajeshgu <rajeshlkh2020@gmail.com>
2026-01-04 01:58:16 +0530

    Add cors creditials

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,11 @@ const router = require("./MCR/routes/admin_route");
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}),
+)
 app.use("/api/v1", router)
 
 
